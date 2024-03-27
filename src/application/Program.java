23853ca27360ead4247f3e0b185305a6870ce1d4
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2024-03-27 10:08:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,23 @@
 package application;
 
+
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		
 		Department obj = new Department(1, "books");
+		
+		Seller seller = new Seller(21, "Rodrigo", "bob@", new Date(), 3000.0, obj);
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
