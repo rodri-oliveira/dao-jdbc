cb62cf6104e0a38b8e735b52d03527a039042614
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2024-03-29 20:18:45 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,8 @@
 package application;
-
-
-import java.util.Date;
 import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -15,14 +11,11 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		
-		Department obj = new Department(1, "books");
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(21, "Rodrigo", "bob@", new Date(), 3000.0, obj);
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
-
 	}
 
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 public class Seller implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
 	private String name;
 	private String email;
